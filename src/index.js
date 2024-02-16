510045e1b1500ab723a2b6b791c8a37c9b27c7bc
slaughtbear <ivanaguirre368@gmail.com>
2024-02-16 10:44:33 -0600

    Reconfigurando Tailwin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react' // Biblioteca de React
 import ReactDOM from 'react-dom/client' // Biblioteca de React que manipula el DOM
+import './index.css'; // Estilos de tailwind
 
 // Elemento raíz para inicializar una app en React
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 function Componente(){
-    return <h1>Hola Mundo</h1>
+    return <h1 className='text-blue-500'>Hola Mundo</h1>
 }
 
 root.render(
