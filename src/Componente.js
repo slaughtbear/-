942c8a8e71957bdb564adec21ffe8113d87652b1
slaughtbear <ivanaguirre368@gmail.com>
2024-02-17 09:18:06 -0600

    pruebas

---
+++ src/Componente.js
@@ -0,0 +1,9 @@
+function Componente() {
+  return (
+    <div>
+      <h1>Componente exportado</h1>
+    </div>
+  );
+}
+
+export default Componente
--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import React from "react"; // Biblioteca de React
 import ReactDOM from "react-dom/client"; // Biblioteca de React que manipula el DOM
 import "./index.css"; // Estilos de tailwind
+import Componente from "./Componente"
 
 // Elemento raíz para inicializar una app en React
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function Componente() {
-  function add(x, y) {
-    return x + y;
-  }
-
-  return <h1>{add(660, 6)}</h1>;
-}
 
 root.render(
   <>
-    <Componente />
-    <Componente />
-    <Componente />
-    <Componente />
+    <Componente/>
   </>
 );
