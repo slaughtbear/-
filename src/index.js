0323da38e7b937b2e2a2902ed16b4348590ab378
slaughtbear <ivanaguirre368@gmail.com>
2024-02-16 19:23:41 -0600

    aprendiendo react

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react' // Biblioteca de React
-import ReactDOM from 'react-dom/client' // Biblioteca de React que manipula el DOM
-import './index.css'; // Estilos de tailwind
+import React from "react"; // Biblioteca de React
+import ReactDOM from "react-dom/client"; // Biblioteca de React que manipula el DOM
+import "./index.css"; // Estilos de tailwind
 
 // Elemento raíz para inicializar una app en React
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function Componente(){
-    return <h1 className='text-blue-500'>Hola Mundo</h1>
+function Componente() {
+  function add(x, y) {
+    return x + y;
+  }
+
+  return <h1>{add(660, 6)}</h1>;
 }
 
 root.render(
-    <Componente/>
-)
+  <>
+    <Componente />
+    <Componente />
+    <Componente />
+    <Componente />
+  </>
+);
